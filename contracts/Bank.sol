06aeba05a5cb8c9d405e4b111a5e4b67c86f6d63
Anubhuti Mittal <mitta139@purdue.edu>
2024-10-06 15:23:22 -0400

    Update Bank.sol
    submitting code for Bank.sol

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,4 +1,4 @@
-// SPDX - License - Identifier : MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 contract Bank {
@@ -7,17 +7,32 @@ contract Bank {
         uint256 balance;
     }
 
-    mapping ( address => Account ) public accounts ;
+    mapping(address => Account) public accounts;
 
-    function createAccount () public {
-        // TODO : Implement the logic to create an account
+    function createAccount() public {
+        // check if account already exist
+        require(accounts[msg.sender].owner == address(0), "Account already exists");
+
+        // create account if does not already exist
+        accounts[msg.sender] = Account(msg.sender, 0);
     }
 
-    function deposit ( uint256 amount ) public {
-        // TODO : Implement the logic to deposit an amount
+    function deposit(uint256 amount) public {
+        // check if account has been created
+        require(accounts[msg.sender].owner != address(0), "Account not created");
+
+        // increase the account balance if account exists
+        accounts[msg.sender].balance += amount;
     }
 
-    function withdraw ( uint256 amount ) public {
-        // TODO : Implement the logic to withdraw an amount
+    function withdraw(uint256 amount) public {
+        // check if account has been created
+        require(accounts[msg.sender].owner != address(0), "Account not created");
+
+        // check if there is enough balance to withdraw
+        require(accounts[msg.sender].balance >= amount, "Insufficient balance");
+
+        // decrease the account balance if account exists
+        accounts[msg.sender].balance -= amount;
     }
 }
